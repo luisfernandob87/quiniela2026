fdeddf34fcb88ac74785996bbe4b972338e1a7f1
Luis Morales <luisfernandob87@gmail.com>
2026-05-17 16:58:30 -0600

    add caché

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRIY5wwgatGCBucjkuIQJ13Dkynw4dmi0",
@@ -13,4 +13,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager()
+  })
+});
